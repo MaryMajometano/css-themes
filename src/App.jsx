9349f1c77264fa66ad0adb7fab93cc49783dd8b5
Mary Majometano <mm@convoyinteractive.com>
2022-07-11 10:14:46 +0200

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Card from "./Card"
+
+
+function App() {
+  return (
+    <div className="space-y-10 p-10">
+      <Card className="" />
+      <Card className="theme-d" />
+      <Card className="theme-s" />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+function customColor(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+        transparent: 'transparent',
+        currentColor: 'currentColor',
+        inherit: 'inherit',
+    },
+  extend: {
+      fontFamily: {
+        sans: ['Roboto Condensed', ...defaultTheme.fontFamily.sans],
+        serif: ['Roboto Slab', ...defaultTheme.fontFamily.serif],
+      },
+      textColor: {
+        skin: {
+          'primary': customColor('--text-primary'),
+          'secondary': customColor('--text-secondary'),
+          'button-primary': customColor('--button-primary-text'),
+          'button-primary-hover': customColor('--button-primary-text-hover'),
+          'button-secondary': customColor('--button-secondary-text'),
+          'button-secondary-hover': customColor('--button-secondary-text-hover'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+            fill: customColor('--background'),
+            'button-primary': customColor('--button-primary-bg'),
+            'button-primary-hover': customColor('--button-primary-bg-hover'),
+            'button-secondary': customColor('--button-secondary-bg'),
+            'button-secondary-hover': customColor('--button-secondary-bg-hover'),
+        },
+      },
+      borderColor: {
+        skin: {
+            DEFAULT: customColor('--border'),
+        },
+      },
+      boxShadowColor: {
+        skin: {
+            DEFAULT: customColor('--shadow'),
+        }
+      },
+    },
+  },
+  plugins: [],
+}
