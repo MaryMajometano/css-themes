23ca69b770535e1590f707e511dae4ecf4a31fb8
Mary Majometano <mm@convoyinteractive.com>
2022-07-18 14:14:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./Card"
 
 function App() {
   return (
-    <div className="space-y-10 p-10">
+    <div className="flex space-x-10 p-10">
       <Card className="" />
       <Card className="theme-d" />
       <Card className="theme-s" />
